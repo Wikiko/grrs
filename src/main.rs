57fa94fefc5f2d80d176b83f234d10c568430b18
William Azevedo <willianray@gmail.com>
2020-08-31 10:37:29 -0300

    atualizando

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::fs::File;
+use std::io::{BufReader, BufRead, Read};
+use std::io;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> io::Result<()> {
+    let args: Cli = Cli::from_args();
+    let file = File::open(&args.path)?;
+    let mut reader = BufReader::new(file);
+
+    for line in reader.by_ref().lines() {
+        let line = line?;
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
 }
