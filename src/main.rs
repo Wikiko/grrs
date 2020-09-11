0d373073e1465df6034cc5a325e9a374f96893f2
William Azevedo <willianray@gmail.com>
2020-09-11 15:04:22 -0300

    Melhoria nas saidas do cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use structopt::StructOpt;
 use std::fs::File;
-use std::io::{BufReader, BufRead, Read};
+use std::io::{BufReader, BufRead, Read, Write};
 use std::io;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -10,15 +12,20 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), ExitFailure> {
     let args: Cli = Cli::from_args();
-    let file = File::open(&args.path)?;
+    let file = File::open(&args.path)
+        .with_context(|_| format!("Could not read file `{}`", &args.path.to_str().unwrap()))?;
     let mut reader = BufReader::new(file);
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
 
     for line in reader.by_ref().lines() {
-        let line = line?;
+        let line = line
+            .with_context(|_| format!("Could not read line..."))?;
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            writeln!(handle, "{}", &line)
+                .with_context(|_| format!("Could not write line to terminal"))?;
         }
     }
     Ok(())
